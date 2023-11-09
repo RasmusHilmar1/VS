df9dfc793a60b4ddf9bc1ce9b62d325be8b960a9
RasmusHilmar1 <142843432+RasmusHilmar1@users.noreply.github.com>
2023-11-09 13:25:01 +0100

    Start

---
+++ cpr.c
@@ -0,0 +1,5 @@
+//
+// Created by Rasmus Hilmar on 09/11/2023.
+//
+
+#include "cpr.h"
---
+++ cpr.h
@@ -0,0 +1,8 @@
+//
+// Created by Rasmus Hilmar on 09/11/2023.
+//
+
+#ifndef VS_CPR_H
+#define VS_CPR_H
+
+#endif //VS_CPR_H
