4a5572de9624d6fdae54d4638925f51eacbf1062
tobi7602 <tkoch23@student.aau.dk>
2023-11-13 12:12:56 +0100

    Enter CPR
    Enter CPR

--- main.c
+++ main.c
@@ -2,13 +2,11 @@
 #include "login.h"
 #include "patient information.h"
 #include "dosage.h"
+#include "cpr.h"
 
 int main() {
     // Take CPR input and print info from user.pass
-    char cpr[10];
-    printf("Input CPR\n>");
-    scanf("%s", cpr);
-    print_patient(cpr);
+    EnterCPR();
 
     liquidDosage();
 }
